1834f41c8781d3be46819fab28f1178b12a5ce8f
Deromir Neves Jr <deromir.neves@hummingbird.com.br>
2017-04-23 12:11:03 -0300

    Initial Commit

---
+++ VerticalBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VerticalBar
+//
+//  Created by Deromir Neves Jr on 23/04/2017.
+//  Copyright © 2017 Deromir Neves Jr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VerticalBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VerticalBar
+//
+//  Created by Deromir Neves Jr on 23/04/2017.
+//  Copyright © 2017 Deromir Neves Jr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
